64965f73660e3850f74bcdab80da2158a5cd5be0
kyo123 <823963923@qq.com>
2017-06-30 17:46:21 +0800

    aaa

--- app/src/main/java/com/kyo/ddl/HomeActivity.java
+++ app/src/main/java/com/kyo/ddl/HomeActivity.java
@@ -5,9 +5,6 @@ import com.kyo.ddl.view.base.KyoBaseActivity;
 
 public class HomeActivity extends KyoBaseActivity {
 
-    /*
-    我是注解。。。。。
-     */
     @Override
     protected int getLayoutResId() {
         return R.layout.activity_home;
